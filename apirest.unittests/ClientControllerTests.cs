5937ec14ea6d52c9d15a06ec84f4ea1fde5a3976
Diego Figueroa <diego.figueroa@digitaldata.com.co>
2025-01-31 08:15:25 -0500

    Revert "minor changes"
    This reverts commit cd31023189bdbdc2d9e0250b4ba3cb7936b8afe0.

--- apirest.unittests/ClientControllerTests.cs
+++ apirest.unittests/ClientControllerTests.cs
@@ -11,16 +11,42 @@ namespace bcp.yape.bo.services.apirest.Tests
 {
     public class ClientControllerTests
     {
+        private readonly Mock<IClientService> _mockClientService;
+        private readonly ClientController _controller;
 
+        // Constructor para inicializar el mock y el controlador
         public ClientControllerTests()
         {
-            // TODO: DAFZ. TBD
+            _mockClientService = new Mock<IClientService>();
+            _controller = new ClientController(_mockClientService.Object);
         }
 
+        // Prueba para crear un cliente con éxito
         [Fact]
         public async Task CreateClient_ShouldReturnCreatedAtAction_WhenClientIsSuccessfullyCreated()
         {
-            // TODO: DAFZ. TBD
+            // Arrange: Prepara los datos de entrada y el comportamiento del mock
+            var request = new ClientRegistrationRequest
+            {
+                Name = "Juan",
+                LastName = "Pérez",
+                CellPhoneNumber = "67654321",
+                DocumentType = DocumentTypeEnum.IdentityCard,
+                DocumentNumber = "12345678",
+                ReasonOfUse = "Compra"
+            };
+
+            var expectedGuid = Guid.NewGuid();
+            var expectedValidationResult = new ValidationResult(true, "", expectedGuid);
+            _mockClientService.Setup(service => service.AddClientAsync(It.IsAny<ClientRegistrationData>())).Returns(Task.FromResult(expectedValidationResult));
+
+            // Act: Llama al método que deseas probar
+            var result = await _controller.CreateClient(request);
+
+            // Assert: Verifica que el resultado sea el esperado
+            var actionResult = result as CreatedAtActionResult;
+            Assert.NotNull(actionResult);
+            Assert.Equal(201, actionResult.StatusCode);
         }
 
     }
